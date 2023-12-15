ed67c36d2e821265f7c593ec128b09500346a768
MalikSploit <maksucce@gmail.com>
2023-12-15 14:16:36 +0100

    Added Our files

---
+++ Micro_Services/Front/Components/Pages/Login.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MicroServices.Components.Pages;
+
+public partial class Login : ComponentBase
+{
+    private async Task HandleLogin()
+    {
+        //A faire :/
+    }
+}
