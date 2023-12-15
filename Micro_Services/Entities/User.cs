8a282792e04d23cbf63fd6b71a09eb4188482df3
Thibault Villepreux <thibault.villepreux@gmail.com>
2023-12-15 15:31:09 +0100

    verifie user manque check bdd

--- Micro_Services/Entities/User.cs
+++ Micro_Services/Entities/User.cs
@@ -2,7 +2,11 @@
 
 using System.Text.Json.Serialization;
 
+using System;
+using System.Net.Mail;
+
 namespace Entities;
+
 
 public class User
 {
@@ -52,7 +56,40 @@ public static class Extension
 
     public static bool IsPasswordRobust(this string s) => s.Length > 8 && s.ToList().TrueForAll(c => char.IsLetter(c));
 
-    public static bool IsEmailValid(this string s) => s.Contains("@") && s.Contains(".");
+    public static bool IsEmailValid(this string s)
+    {
+        try
+        {
+            // Tentative de création d'une instance de MailAddress
+            MailAddress mailAddress = new MailAddress(s);
+            return true; // Si la création réussit, l'adresse e-mail est valide
+        }
+        catch (FormatException)
+        {
+            return false; // Une exception indique que l'adresse e-mail n'est pas valide
+        }
+    }
 
-    public static bool IsNameValid(this string s) => s.Length > 3 && s.Length < 20 && s.ToList().TrueForAll(c => char.IsLetter(c));
+    public static bool IsNameValid(this string s)
+    {
+        // Vérification de la longueur et des caractères
+        bool isLengthValid = s.Length > 3 && s.Length < 20 && s.All(char.IsLetterOrDigit);
+
+        if (!isLengthValid)
+        {
+            return false; // Le nom ne respecte pas les conditions de longueur ou de caractères
+        }
+
+        // Vérification que le nom n'existe pas dans la base de données
+        bool isNotInDatabase = !IsUsernameInDatabase(s);
+
+        return isNotInDatabase;
+    }
+
+    // Méthode de vérification de l'existence du nom d'utilisateur dans la base de données
+    private static bool IsUsernameInDatabase(string username)
+    {
+        // Code de vérification de l'existence du nom d'utilisateur dans la base de données
+        return false;
+    }
 }
