432b0d02e7dfbaa21632e772995e212ca0997b45
MalikSploit <maksucce@gmail.com>
2023-12-21 22:49:34 +0100

    Implementation complete de la bare de recherche de livres

--- Micro_Services/Front/Components/Pages/Explore.razor.cs
+++ Micro_Services/Front/Components/Pages/Explore.razor.cs
@@ -9,10 +9,19 @@ namespace Front.Components.Pages
         private BookService _BookService { get; set; }
         
         private IEnumerable<Book> books;
+        private IEnumerable<Book> filteredBooks;
+        private string searchTerm = string.Empty;
 
         protected override async Task OnInitializedAsync()
         {
             books = await _BookService.GetBooksAsync();
+            filteredBooks = books;
+        }
+        
+        private void UpdateSearch(ChangeEventArgs e)
+        {
+            searchTerm = e.Value.ToString();
+            filteredBooks = string.IsNullOrWhiteSpace(searchTerm) ? books : books.Where(b => b.Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
