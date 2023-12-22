a802a093e532038dea09b90b2e6f2bee05ba4df6
MalikSploit <maksucce@gmail.com>
2023-12-22 15:50:53 +0100

    Supression de profile et ajout de check dans explore

--- Micro_Services/Front/Components/Pages/Explore.razor.cs
+++ Micro_Services/Front/Components/Pages/Explore.razor.cs
@@ -1,12 +1,16 @@
-﻿using Front.Services;
+﻿using Blazored.LocalStorage;
+using Front.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace Front.Components.Pages
 {
     public partial class Explore : ComponentBase
     {
-        [Inject]
-        private BookService _BookService { get; set; }
+        [Inject] private BookService _BookService { get; set; }
+        [Inject] private ILocalStorageService LocalStorage { get; set; }
+        [Inject] private NavigationManager NavigationManager { get; set; }
+
+        private bool _isLoggedIn;
         
         private IEnumerable<Book> books;
         private IEnumerable<Book> filteredBooks;
@@ -16,12 +20,25 @@ namespace Front.Components.Pages
         {
             books = await _BookService.GetBooksAsync();
             filteredBooks = books;
+            _isLoggedIn = await IsUserLoggedIn();
         }
-        
+
+        private async Task<bool> IsUserLoggedIn()
+        {
+            var jwtToken = await LocalStorage.GetItemAsStringAsync("jwtToken");
+            return !string.IsNullOrEmpty(jwtToken);
+        }
+
         private void UpdateSearch(ChangeEventArgs e)
         {
             searchTerm = e.Value.ToString();
             filteredBooks = string.IsNullOrWhiteSpace(searchTerm) ? books : books.Where(b => b.Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase));
+        }
+        
+        private async Task Logout()
+        {
+            await LocalStorage.RemoveItemAsync("jwtToken");
+            NavigationManager.NavigateTo("/", true);
         }
     }
 }
