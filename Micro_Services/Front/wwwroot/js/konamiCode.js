a5c55f1bc750a516cb52134111fc611390d555fb
MalikSploit <maksucce@gmail.com>
2024-01-15 10:09:54 +0100

    Add Konami code sequence and confetti effect
    A new JavaScript file, konamiCode.js, has been created that implements the Konami code sequence. When correctly inputted, a confetti effect is triggered. For this, the Canvas-confetti library was added through a script tag in App.razor. These fun additions aim to enhance user interaction and engagement.

---
+++ Micro_Services/Front/wwwroot/js/konamiCode.js
@@ -0,0 +1,25 @@
+﻿let konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let current = 0;
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === konamiCode[current]) {
+        current++;
+
+        if (current === konamiCode.length) {
+            triggerConfetti();
+            current = 0;
+        }
+    } else {
+        current = 0;
+    }
+});
+
+function triggerConfetti() {
+    confetti({
+        particleCount: 200,   // Increase the number of particles
+        spread: 90,           // Increase the spread angle
+        startVelocity: 50,    // Increase the starting velocity
+        decay: 0.95,          // To make particles float longer
+        origin: { y: 1 }      // Adjust origin
+    });
+}
